ccd32382b143269151101c0677f7e34c3b0841bc
noppakorn <noppakorn11417555@gmail.com>
2019-10-28 16:03:56 +0700

    Add for peace.

--- kmitlcanteen/urls.py
+++ kmitlcanteen/urls.py
@@ -14,10 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import url
-from django.views.generic import TemplateView
+from django.urls import path
 
 urlpatterns = [
-    url('admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('admin/', admin.site.urls),
 ]
