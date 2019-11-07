685a832386d2cf9c57cb6c26d07c128fbec87dc7
noppakorn <noppakorn11417555@gmail.com>
2019-11-07 19:12:25 +0700

    About something

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app light>
+  <v-app dark>
     <v-navigation-drawer
       v-model="drawer"
       :mini-variant="miniVariant"
@@ -24,18 +24,18 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" fixed app>
+    <v-app-bar :clipped-left="clipped" fixed app elevate-on-scroll>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-toolbar-title v-text="title" class="mx-auto" />
+      <v-toolbar-title class="mx-auto" v-text="title" />
     </v-app-bar>
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
-    <v-footer :fixed="fixed" app>
+    <!-- <v-footer :fixed="fixed" app>
       <span>&copy; 2019</span>
-    </v-footer>
+    </v-footer> -->
   </v-app>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,18 @@
 <template>
-  <div></div>
+  <div>
+    <Feature />
+    <v-row>
+      <v-col display-1 justfy-center>
+        <p>Ahh..</p>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
-export default {}
+import Feature from '@/components/homepage/Feature'
+
+export default {
+  Feature
+}
 </script>
