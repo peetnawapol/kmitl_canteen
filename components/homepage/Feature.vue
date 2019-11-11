6d32f1d38220fb4c04d7fe0c60ca5a2b2261c1c1
noppakorn <noppakorn11417555@gmail.com>
2019-11-11 17:03:23 +0700

    Change Homepage design

--- components/homepage/Feature.vue
+++ components/homepage/Feature.vue
@@ -1,7 +1,13 @@
 <template>
-  <div>
-    <!-- <b-img-lazy src="@/static/wall1.jpg" /> -->
+<div>
+  <div class="container pt-5">
+    <div class="row">
+      <div class="col-md-12 d-flex justify-content-center align-items-center">
+        <h1 class="text-white">HERE'S KMITL CANTEEN</h1>
+      </div>
+    </div>
   </div>
+</div>
 </template>
 <style scoped>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="background">
     <Feature />
   </div>
 </template>
